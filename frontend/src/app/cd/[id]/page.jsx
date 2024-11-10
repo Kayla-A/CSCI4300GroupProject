a37aae5c4df7f472c76bbdeb835852f377fb6d5f
Kayla-A <kayla.ka.arthur@outlook.com>
2024-11-10 17:20:52 -0500

    started view cd information section on cd/[id]/page.jsx

---
+++ frontend/src/app/cd/[id]/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+import strict from "assert/strict";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+// The CD's data to display
+interface CDData {
+    name: string;
+    artist: string;
+    date: string;
+    imageURL: String;
+    tracklist: string[];
+} // CDData
+
+const CDDetails = () => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    // Dummy data for now !!!!!!!!!!!!!!!!!!!!!!
+    const [cdData, setCdData] = useState<CDData> ({
+        name: "Dummy Album",
+        artist: "Dummy Artist",
+        date: "dumbuaray/04/2024",
+        imageUrl: "https://pbs.twimg.com/profile_images/1584626935946104833/PG6hu59k_400x400.jpg",
+        tracklist: ["You", "Are", "A", "Dummy", "Just Kidding", "LOL"],
+    });
+
+    useEffect(() => {
+        // fetch CD data using id if API avaliable 
+        // don't have for now so use dummy data
+    }, [id]);
+
+    return (
+        <div className = "cdInfoContainer">
+             <NavBar></NavBar>
+            {/*{!isLoggedIn && (
+                <p>NOT LOGGED IN</p>
+            )}*/}
+            <div className = "cdInfoLeftSide">
+                <h1> {cdData.name} </h1>
+                <p><strong> Artist: </strong> {cdData.artist} </p>
+                <p><strong> Release Data: </strong> {cdData.data} </p>
+                {cdData.imageUrl && (
+                    <img src={cdData.imageUrl} alt={'${cdData.name} cover'} />
+                )}
+            </div>
+            <div className = "cdInfoRightSide">
+                <h2> Track List: </h2>
+                <ul>
+                    {cdData.tracklist.map((track, index) => (
+                        <li key={index}>{track}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}; //CDDetails
+
+export default CDDetails
+
