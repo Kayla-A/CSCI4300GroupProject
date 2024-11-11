338b3c976af665243e0b3309c00c173d15f88e38
Kayla-A <kayla.ka.arthur@outlook.com>
2024-11-10 23:32:11 -0500

    Added dummy array

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import Nav from "./components/navBar"
 import {AuthContext} from "./context/user";
 import Link from "next/link";
 import {useContext} from "react"
+import Item from "./components/items" //!!!!!!
 const Home = () => {
     const context = useContext(AuthContext);
 
@@ -13,6 +14,14 @@ const Home = () => {
 
     const { isLoggedIn, logout } = context;
 
+    // dummy array !!!!!!!!!!!!!!!!!!!
+    const items = [
+        {id: 1, image: "https://qodeinteractive.com/magazine/wp-content/uploads/2020/06/8-Tyler-the-Creator.jpg", title: "Item 1"},
+        {id: 2, image: "https://www.billboard.com/wp-content/uploads/media/ariana-grande-sweetner-album-art-2018-billboard-1240.jpg?w=600", title: "Item 2"},
+        {id: 3, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6qh_FaKYHyGpJccXuuYs5Y7JULJetgAuk1w&s", title: "Item 3"}
+    ];
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
     return (
         <div>
@@ -21,6 +30,13 @@ const Home = () => {
                 <div className="flex flex-col items-center justify-center min-h-screen gap-5">
                     <Image src="/cd.png" alt="cd" width={500} height={500}/>
                     <h1 className="text-black text-5xl">Shelf Space</h1>
+                    {/* testing dummy array */}
+                    <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+                        {items.map(item => (
+                            <Item key={item.id} image={item.image} title={item.title} />
+                        ))}
+                    </div>
+                    {/* testing dummy array */}
                     <p className="text-black">See your Cd collection wherever whenever</p>
                     <div className="flex items-center mx-auto gap-4 ">
                         <Link href="./login">
