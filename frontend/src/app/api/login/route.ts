ab3858ed1fe54e85422303b4dc1f32a549bc7c8d
ishanipatil624 <ikp53692@uga.edu>
2024-11-15 11:00:50 -0500

    had to fix login api

--- frontend/src/app/api/login/route.ts
+++ frontend/src/app/api/login/route.ts
@@ -9,16 +9,18 @@ export async function POST(request: NextRequest) {
     const {username, password} = await request.json();
     await connectMongoDB();
     //find user via username
-    const user = await User.findOne({username:{username}});
+    const user = await User.findOne({ username });
     if(!user){
         return NextResponse.json({message:"Username incorrect"}, {status: 400});
     }
     const realPass = user.password;
     //compare hash
-    if (!bcrypt.compareSync({password}, {realPass})) {
-        return NextResponse.json({message:"Password incorrect"}, {status: 400});
+    const isMatch = await bcrypt.compare(password, realPass);
+    if (!isMatch) {
+        return NextResponse.json({ message: "Password incorrect" }, { status: 400 });
     }//if pass wrong
-    return NextResponse.json({user}, {status: 201});
+    const { password: _, ...userData } = user.toObject(); // removes password
+    return NextResponse.json({ message: "login verified!",user: userData }, { status: 200 });
 }
 
 
