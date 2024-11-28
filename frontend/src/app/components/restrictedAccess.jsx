a5e6186f473ca7fb2259c158626203a4ba6b5567
ishanipatil624 <ikp53692@uga.edu>
2024-11-27 20:11:23 -0500

    chnaged restricted access pg

--- frontend/src/app/components/restrictedAccess.jsx
+++ frontend/src/app/components/restrictedAccess.jsx
@@ -5,7 +5,7 @@ export default function RestrictedAccess(){
     return(
         <div className="p-2">
             <title className="text-7xl">Restricted Access</title>
-            <p>You cannot access this page because you are not logged in.</p>
+            <p>You cannot access this page.</p>
             <Link href="../">
                 <button className="rounded bg-red-500 text-white px-4 py-3 hover:bg-white hover:text-black">
                     Return to home
